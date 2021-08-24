e99a29e3e61533172415849aa105f263de52c3f1
RaphaelS1 <raphaelsonabend@gmail.com>
2021-08-24 17:00:30 +0100

    export private

--- R/private.R
+++ R/private.R
@@ -1,4 +1,8 @@
+#' Access the private environment of an R6 object
+#' @title Get R6 object private environment
+#' @param x (`R6`) \cr R6 object to get environment from, errors if not R6
+#' @export
 private <- function(x) {
-  stopifnot(is.R6(x))
+  stopifnot(is.R6Object(x))
   x$.__enclos_env__$private
 }
